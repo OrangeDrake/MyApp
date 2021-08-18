15100133c7f9becf836b25808e53ae869e3d905d
tesr108 <88037259+tesr108@users.noreply.github.com>
2021-08-18 14:17:38 +0200

    2

---
+++ BlazorBattles/Client/Shared/UserUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Shared
+{
+    public class UserUnit
+    {
+        public int UserID { get; set; }
+        public int UnitID { get; set; }
+        public int HitPoints { get; set; }
+    }
+}
