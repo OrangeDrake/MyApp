a40e9e9eba78c05a771e36b2536d0a16b4a88d10
tesr108 <88037259+tesr108@users.noreply.github.com>
2021-08-23 09:14:55 +0200

    4

--- BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorBattles.Client.Shared;
+using Blazored.Toast.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,16 @@ namespace BlazorBattles.Client.Services
 {
     public class UnitService : IUnitService
     {
+        private readonly IToastService _toastService;
+        public UnitService (IToastService toastService)
+        {
+            _toastService = toastService;
+        }
         public IList<Unit> Units => new List<Unit>
         {
             new Unit { Id = 1, Title = "Knight", Attack = 10, Defense = 10, BanaCost = 100 },
-            new Unit { Id = 1, Title = "Archr", Attack = 15, Defense = 5, BanaCost = 150 },
-            new Unit { Id = 1, Title = "MAeg", Attack = 20, Defense = 1, BanaCost = 200 }
+            new Unit { Id = 2, Title = "Archr", Attack = 15, Defense = 5, BanaCost = 150 },
+            new Unit { Id = 3, Title = "Mag", Attack = 20, Defense = 1, BanaCost = 200 }
         };
 
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
@@ -20,7 +26,11 @@ namespace BlazorBattles.Client.Services
         public void AddUnit(int unitId)
         {
             var unit = Units.First(unit => unit.Id == unitId);
-            MyUnits.Add(new UserUnit { UnitID = unit.Id, HitPoints = unit.HitPoints });
+            MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
+            _toastService.ShowSuccess($"Your {unit.Title} has been buildt!");
+
+            //Console.WriteLine($"{unit.Title} was build!");
+            //Console.WriteLine($"Your army size: {MyUnits.Count}");
         }
     }
 }
---
+++ BlazorBattles/Client/Shared/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage ="Please enter the name")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
