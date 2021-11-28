aa7b6065bc07cd32f69ed9502fd1f851c05d804d
OrangeDrake <petr.fiala@atlasa.cz>
2021-11-29 00:08:27 +0100

    when editing goal boundaries (start and end day)  for editing is no possible move

---
+++ BlazorBattles/Client/Model/MoveBoundaries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Model
+{
+    public class MoveBoundaries
+    {
+        public DateTime startCalendarDay { get; set; }
+        public DateTime endCalendarDay { get; set; }
+
+    }
+}
